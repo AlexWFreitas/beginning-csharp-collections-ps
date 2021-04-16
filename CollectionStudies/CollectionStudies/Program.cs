0fc37d512a766b1f796b56060a1a08cfd0d85e74
Alex W. de Freitas <otaru1921@gmail.com>
2021-04-16 14:57:24 -0300

    Starting Chapter 2 - First Code

---
+++ CollectionStudies/CollectionStudies/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionStudies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            Console.WriteLine(daysOfWeek.Length);
+        }
+    }
+}
