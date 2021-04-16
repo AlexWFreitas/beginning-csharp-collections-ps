51d70eea10e79d273986bb0f73840584a3261ae0
Alex W. de Freitas <otaru1921@gmail.com>
2021-04-16 16:22:29 -0300

    90% of Chapter 2.

--- CollectionStudies/CollectionStudies/Program.cs
+++ CollectionStudies/CollectionStudies/Program.cs
@@ -14,14 +14,29 @@ namespace CollectionStudies
             {
                 "Monday",
                 "Tuesday",
-                "Wednesday",
+                "Wensday",
                 "Thursday",
                 "Friday",
                 "Saturday",
                 "Sunday"
             };
 
-            Console.WriteLine(daysOfWeek.Length);
+            Console.WriteLine("Before: ");
+            foreach (string day in daysOfWeek)
+                Console.WriteLine(day);
+
+            daysOfWeek[2] = "Wednesday";
+
+            Console.WriteLine("\r\nAfter: ");
+            foreach (string day in daysOfWeek)
+                Console.WriteLine(day);
+
+            //Console.WriteLine("Which day do you want to display?");
+            //Console.Write("(Monday = 1, etc. ) > ");
+            //int iDay = int.Parse(Console.ReadLine());
+
+            //string chosenDay = daysOfWeek[iDay - 1];
+            //Console.WriteLine($"That day is {chosenDay}");
         }
     }
 }
