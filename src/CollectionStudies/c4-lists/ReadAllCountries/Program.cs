647bc5ccc9e6536f7c0170dd66882f493e42ebd7
Alex W. de Freitas <otaru1921@gmail.com>
2021-04-19 17:38:49 -0300

    Chapter 4 - Done - Much Writing WoW

--- src/CollectionStudies/c4-lists/ReadAllCountries/Program.cs
+++ src/CollectionStudies/c4-lists/ReadAllCountries/Program.cs
@@ -13,11 +13,17 @@ namespace ReadAllCountries
 
             List<Country> countries = reader.ReadAllCountries();
 
+            Country lilliput = new Country("Lilliput", "LIL", "Somewhere", 2_000_000);
+            int lilliputIndex = countries.FindIndex(country => country.Population < 2_000_000);
+            countries.Insert(lilliputIndex, lilliput);
+            countries.RemoveAt(lilliputIndex);
+
             foreach (Country country in countries)
             {
                 Console.WriteLine($@"{PopulationFormatter.FormatPopulation
                     (country.Population).PadLeft(15)} : {country.Name}");
             }
+            Console.WriteLine($"{(countries.Count).ToString().PadLeft(15)} : Countries on this List");
         }
     }
 }
