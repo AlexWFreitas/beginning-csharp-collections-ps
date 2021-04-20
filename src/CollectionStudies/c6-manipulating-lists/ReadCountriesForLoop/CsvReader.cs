01057a68208d6956407e51730dae2b9ac8153a65
Alex W. de Freitas <otaru1921@gmail.com>
2021-04-20 15:08:52 -0300

    Chapter 6 - Started - Cleaned gitignore

---
+++ src/CollectionStudies/c6-manipulating-lists/ReadCountriesForLoop/CsvReader.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace ReadCountriesForLoop
+{
+    class CsvReader
+    {
+        private string _csvFilePath;
+
+        public CsvReader(string csvFilePath)
+        {
+            this._csvFilePath = csvFilePath;
+        }
+
+        public List<Country> ReadAllCountries()
+        {
+            List<Country> countries = new List<Country>();
+
+            using (StreamReader sr = new StreamReader(_csvFilePath))
+            {
+                // Reads header line - to skip column names
+                sr.ReadLine();
+
+                string csvLine;
+
+                while ((csvLine = sr.ReadLine()) != null)
+                {   
+                    countries.Add(ReadCountryFromCsvLine(csvLine));
+                }
+            }
+            return countries;
+        }
+
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(',');
+            
+            string name;
+            string code;
+            string region;
+            string popText;
+
+            switch(parts.Length)
+            {
+                case 4:
+                    name = parts[0];
+                    code = parts[1];
+                    region = parts[2];
+                    popText = parts[3];
+                    break;
+
+                case 5:
+                    name = parts[0];
+                    name += parts[1];
+                    code = parts[2];
+                    region = parts[3];
+                    popText = parts[4];
+                    break;
+
+                default:
+                    throw new System.Exception($"Can't parse country from csvLine: {csvLine}");
+            }
+
+            int.TryParse(popText, out int population);
+
+            return new Country(name, code, region, population);
+        }
+    }
+}
---
+++ src/CollectionStudies/c6-manipulating-lists/ReadCountriesForLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReadCountriesForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"F:\Workspace\c-sharp-path\beginning-csharp-collections-ps\src\CollectionStudies\Pop by Largest Final.csv";
+
+            CsvReader reader = new CsvReader(filePath);
+
+            List<Country> countries = reader.ReadAllCountries();
+
+            for (int i = 0; i < countries.Count; i++)
+            {
+                Country country = countries[i];
+                Console.WriteLine($@"{PopulationFormatter.FormatPopulation
+                    (country.Population).PadLeft(15)} : {country.Name}");
+            }
+            Console.WriteLine($"\n{(countries.Count).ToString().PadLeft(15)} : Countries on this List");
+        }
+    }
+}
