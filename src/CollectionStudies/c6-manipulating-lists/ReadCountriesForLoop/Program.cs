a64785e61c8d0760cfd46b80de83908d0895bf23
Alex W. de Freitas <otaru1921@gmail.com>
2021-04-20 15:47:28 -0300

    Batch iterating and displaying order position on program.

--- src/CollectionStudies/c6-manipulating-lists/ReadCountriesForLoop/Program.cs
+++ src/CollectionStudies/c6-manipulating-lists/ReadCountriesForLoop/Program.cs
@@ -13,13 +13,26 @@ namespace ReadCountriesForLoop
 
             List<Country> countries = reader.ReadAllCountries();
 
+            Console.Write("Enter no. of countries to display per batch >");
+            bool inputIsInt = int.TryParse(Console.ReadLine(), out int userInput);
+            if (!inputIsInt || userInput <= 0)
+            {
+                Console.WriteLine("You must type in an integer. Exiting program...");
+                return;
+            }
+
             for (int i = 0; i < countries.Count; i++)
             {
+                if (i > 0 && (i % userInput == 0))
+                {
+                    Console.Write("Hit return to continue, anything else to quit >");
+                    if (Console.ReadLine() != "")
+                        break;
+                }
                 Country country = countries[i];
-                Console.WriteLine($@"{PopulationFormatter.FormatPopulation
+                Console.WriteLine($@"{(i + 1).ToString().PadRight(3)}{PopulationFormatter.FormatPopulation
                     (country.Population).PadLeft(15)} : {country.Name}");
             }
-            Console.WriteLine($"\n{(countries.Count).ToString().PadLeft(15)} : Countries on this List");
         }
     }
 }
