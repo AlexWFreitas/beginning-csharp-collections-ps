26a64519f4c26a64c72756f423edafc9b0403097
Alex W. de Freitas <otaru1921@gmail.com>
2021-04-20 13:10:49 -0300

    Chapter 5 - 90% -  Let's go

---
+++ src/CollectionStudies/c5-dict/DictSnippets/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictSnippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Country norway = new Country("Norway", "NOR", "Europe", 5_282_223);
+            Country finland = new Country("Finland", "FIN", "Europe", 5_511_303);
+
+            Dictionary<string, Country> countries = new Dictionary<string, Country>
+            {
+                { norway.Code, norway },
+                { finland.Code, finland }
+            };
+
+            foreach (Country country in countries.Values)
+                Console.WriteLine(country.Name);
+
+            //Country selectedCountry = countries["NOR"];
+
+            // Explanation about trying to use the value of a key that doesn't exist
+
+            // Will throw exception
+            // Console.WriteLine(countries["MUS"].Name);
+            
+            // Won't throw exception - Because it checks if the key / value exists.
+            {
+                bool exists = countries.TryGetValue("MUS", out Country country);
+                if (exists)
+                    Console.WriteLine("\n" + country.Name);
+                else
+                    Console.WriteLine("\nThere is no country with the code MUS");
+            }
+
+
+
+        }
+    }
+}
